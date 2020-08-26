045f4e7b2d6b921d01e4af6f83492935a4b9b718
sayid <herq12315@gmail.com>
2020-08-26 14:38:24 +0800

    move App.css to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 //import App from "./Api.js";
 import './index.css'
+import "./App.css";
 import img from './images/bg.jpg';
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById("root")
 );
