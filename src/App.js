1aab7d5711b329cdcb93c03978192d59aa353a08
sayid <herq12315@gmail.com>
2021-06-23 17:39:40 +0800

    using hook instead of class

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import {hot} from "react-hot-loader/root";
-import React, { Component} from "react";
-import Table from './components/Table';
-import Form from './components/Form';
+import React from "react";
 import Time from './components/Time';
 import './App.scss';
 
-class App extends Component {
-  state = {
-    characters: [],
-  }
-
-  removeCharacter = (index) => {
-    const {characters} = this.state
-
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i != index
-      }),
-    })
-  }
-
-  handleSubmit = (character) => {
-    this.setState({characters: [...this.state.characters, character]})
-  }
-
-  render(){
-    const { characters } = this.state
-
-    return(
-      <div className="App">
-        <h1>Hello, World!</h1>
-        <h2><Time /></h2>
-        <Table characterData={characters} removeCharacter={this.removeCharacter} />
-        <Form handleSubmit={this.handleSubmit} />
-      </div>
-    );
-  }
+const App = () => {
+  return(
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <h2><Time /></h2>
+    </div>
+  )
 }
 
 export default hot(App);
